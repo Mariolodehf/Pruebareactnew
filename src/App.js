b70a71ee113823070fb041e52a7c82ae1c72f872
Mariolodehf <47900583+Mariolodehf@users.noreply.github.com>
2022-11-10 19:37:38 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Mundito</h1>
+      <h2>Start la edición de algo super mágico!</h2>
+    </div>
+  );
+}
